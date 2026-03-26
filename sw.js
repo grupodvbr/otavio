e2393745344d6f2f60119dda3ea1b5b12be59056
grupodvbr <grupodvbr@gmail.com>
2026-03-26 20:53:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
+const CACHE = "zaapdv-v1"
+
 self.addEventListener("install", e=>{
   self.skipWaiting()
 })
 
 self.addEventListener("fetch", e=>{
-  e.respondWith(fetch(e.request))
+  e.respondWith(
+    fetch(e.request).catch(()=>new Response("Offline"))
+  )
 })
